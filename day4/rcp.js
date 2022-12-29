f1b3e380178060a6f850da464fc73c0f011bb68e
yoongg <yoonna0912@gmail.com>
2022-12-29 16:06:09 +0900

    js(가위바위보 예제)

---
+++ day4/rcp.js
@@ -0,0 +1,8 @@
+function rpsgame(){
+   const rock = document.getElementsByClassName('rock')
+   const papper = document.getElementsByClassName('papper')
+   const scissors = document.getElementsByClassName('scissors')
+
+   const computer_choice = Number(Math.random()*10)
+   
+}
